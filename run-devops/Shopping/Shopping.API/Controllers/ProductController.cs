41d309f2a8984815f24ffe97eddf138040569065
Harpreet Singh <hpslogics@gmail.com>
2022-05-30 20:31:29 +0530

    adding project to git

---
+++ run-devops/Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ProductContext _productContext;
+
+        public ProductController(ProductContext productContext, ILogger<ProductController> logger)
+        {
+            _productContext = productContext;
+            _logger = logger;
+        }
+
+        private readonly ILogger<ProductController> _logger;
+
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _productContext.Products.Find(p => true).ToListAsync();
+
+        }
+    }
+}
